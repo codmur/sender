7320f3f897482c80458a6ff6f657803c1ee3498f
juan <jpenalver@jnc.es>
2025-02-23 16:57:42 +0100

    add more logs

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,10 +4,12 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:5173/',
   'https://victoriaseguros.com',
   'http://victoriaseguros.com',
+  '*'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
+    console.log(origin)
     if (acceptedOrigins.includes(origin)) {
       return callback(null, true)
     }
