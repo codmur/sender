046ca78950feeb7de28d06c2ae9c525a19a1332d
juan <jpenalver@jnc.es>
2025-02-23 17:50:36 +0100

    add cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,8 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
   'https://victoriaseguros.com',
   'http://victoriaseguros.com',
-  'https://bespoke-tartufo-b225e9.netlify.app'
+  'https://bespoke-tartufo-b225e9.netlify.app',
+  'https://bucolic-youtiao-095230.netlify.app'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
